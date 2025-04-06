088df38932b3ff9e08650d0d5f5e714aaf844ab8
Milagros Sofìa Pèrez <mimisofiap21@gmail>
2025-04-06 16:10:27 -0300

    Agregue la funcion para detectar que compu es la mas vieja

--- tp2_1_4.c
+++ tp2_1_4.c
@@ -13,6 +13,7 @@ typedef struct
 } compus;
 
 void listarPCs(compus pcs[], int cantidad);
+void mostrarMasVieja(compus pcs[], int cantidad);
 
 int main()
 {   
@@ -40,6 +41,7 @@ int main()
 
     listarPCs(compu, 5);
     
+    mostrarMasVieja(compu, 5);
     
     return 0;
 }
@@ -57,4 +59,36 @@ void listarPCs(compus pcs[], int cantidad){
         printf("\n");
     }
     
+}
+
+void mostrarMasVieja(compus pcs[], int cantidad){
+    //Recibe el arreglo de PCs y la cantidad de elementos.
+    //Busca la PC con el menor año de fabricación (la más vieja).
+    //Muestra por pantalla las características de la PC más vieja encontrada. Si hay varias con el mismo año más antiguo, puedes mostrar la primera que encuentres.
+
+    int anioViejo = 7000;
+    int compuVieja = 0;
+
+    for (int i = 0; i < cantidad; i++)
+    {    
+
+        if (pcs[i].anio < anioViejo)
+        {
+            anioViejo = pcs[i].anio;
+            compuVieja = i;
+        }
+    }
+
+    printf("\n -----------------------------------");
+    printf("\n Computadora %d", compuVieja+1);
+    printf("\n  -------- DATOS --------");  
+    printf("\n   #Velocidad: %d", pcs[compuVieja].velocidad);
+    printf("\n   #Anio: %d", pcs[compuVieja].anio);
+    printf("\n   #Cantidad de nucleos: %d", pcs[compuVieja].cantidad_nucleos);
+    printf("\n #Tipo CPU: %s", pcs[compuVieja].tipo_cpu);
+    printf("\n");
+
+    
+
+
 }
